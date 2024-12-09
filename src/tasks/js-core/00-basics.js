d8237479b87dce8372a694664432ff129e1de6d6
Hanik <hanikn20@gmail.com>
2024-12-09 21:47:10 +0200

    Basics done

--- src/tasks/js-core/00-basics.js
+++ src/tasks/js-core/00-basics.js
@@ -10,6 +10,13 @@ function sum(a, b, c) {
 function safeSum(a, b, c) {
   let sum = 0;
   //your code should be here
+  let aNum = Number(a);
+  let bNum = Number(b);
+  let cNum = Number(c);
+
+  if (!isNaN(aNum)) sum += aNum;
+  if (!isNaN(bNum)) sum += bNum;
+  if (!isNaN(cNum)) sum += cNum;
 
   return sum;
 }
@@ -37,6 +44,9 @@ function min(a, b, c) {
 // do not use Number.isInteger
 function isInteger(n) {
   //your code is here
+  if (n % 1 === 0) {
+    return true;
+  } else return false;
 }
 
 // write logic for rounding given number n with given precision k
@@ -45,12 +55,38 @@ function isInteger(n) {
 // do not use Number.toFixed
 function roundNumber(n, k) {
   //your code is here
+  const numParts = n.toString().split("."); //["0", "77777"]
+
+  if (numParts.length < 2 || numParts[1].length < k) {
+    return n;
+  }
+
+  let arr = numParts[1].split("");
+
+  const checkNum = parseInt(arr[k]);
+  let roundNum = parseInt(arr[k - 1]);
+
+  if (checkNum >= 5) {
+    roundNum += 1;
+  }
+
+  arr[k - 1] = roundNum.toString();
+  let updatedPart = arr.slice(0, k).join("");
+
+  return parseFloat(`${numParts[0]}.${updatedPart}`);
 }
 
 // given array of numbers
 // return percentages of each number in array
 // it mean how many percents each number take from total sum
-function percentage(numbers) {}
+function percentage(numbers) {
+  let sum = 0;
+  numbers.forEach((number) => {
+    sum += number;
+    (number / sum) * 100;
+  });
+  return numbers.map((number) => (number / sum) * 100);
+}
 
 module.exports = {
   sum,
